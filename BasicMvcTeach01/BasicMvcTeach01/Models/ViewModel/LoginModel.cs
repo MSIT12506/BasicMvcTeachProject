0e05d32cf23aaaeae4e0af22515d5299543720a8
Yuan <anm821@gmail.com>
2020-02-22 14:41:49 +0800

    新增登入用的檢視模型( Login Model ) 與 登入的檢視(View)

---
+++ BasicMvcTeach01/BasicMvcTeach01/Models/ViewModel/LoginModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BasicMvcTeach01.Models.ViewModel
+{
+    public class LoginModel
+    {
+
+        [DisplayName("帳號")]
+        [Required(ErrorMessage = "請輸入帳號")]
+        [StringLength(12, MinimumLength = 6, ErrorMessage = "帳號長度 6 ~ 12 碼之間")]
+        public string Account { get; set; }
+
+        [DisplayName("密碼")]
+        [Required(ErrorMessage = "請輸入密碼")]
+        public string Password { get; set; }
+
+        [DisplayName("驗證碼")]
+        public string OtpKey { get; set; }
+    }
+}
